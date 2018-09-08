816d50eb431cf857477f96163da9849c4cd42dc6
Michael Salvati <print@3daddict.com>
2018-09-08 16:56:14 -0700

    react html and css styles added

--- app.jsx
+++ app.jsx
@@ -1,7 +1,53 @@
 function Application() {
-  return (
+return (
+<div className="scoreboard">
+  <div className="header">
+    <h1>Scoreboard</h1>
+  </div>
 
-  );
+  <div className="players">
+    <div className="player">
+      <div className="player-name">
+        Michael Salvati
+      </div>
+      <div className="player-score">
+        <div className="counter">
+          <button className="counter-action decrement">
+            -
+          </button>
+          <div className="counter-score">
+            31
+          </div>
+          <button className="counter-action increment">
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div className="player">
+      <div className="player-name">
+        Boobie Harris
+      </div>
+      <div className="player-score">
+        <div className="counter">
+          <button className="counter-action decrement">
+            -
+          </button>
+          <div className="counter-score">
+            20
+          </div>
+          <button className="counter-action increment">
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+);
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(
+<Application />, document.getElementById('container'));
