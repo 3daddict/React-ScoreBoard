8ce0be574b2c4aa0ceb1ea58dad4740f27c9e1c0
Michael Salvati <print@3daddict.com>
2018-09-10 09:25:11 -0700

    scoreboard button function with index and delta

--- app.jsx
+++ app.jsx
@@ -31,15 +31,17 @@ Header.propTypes = {
 function Counter(props){
   return (
     <div className="counter">
-      <button className="counter-action decrement" >-</button>
+      <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}>-</button>
       <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment" >+</button>
+      <button className="counter-action increment" onClick={function() {props.onChange(1);}}>+</button>
     </div>
   );
 }
 
 Counter.propTypes = {
-  score: React.PropTypes.number.isRequired
+  score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  onScoreChange: React.PropTypes.func.isRequired,
 }
 
 function Player(props) {
@@ -49,7 +51,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score}/>
+        <Counter score={props.score} onChange={props.onScoreChange} />
       </div>
     </div>
   );
@@ -81,6 +83,12 @@ var Application = React.createClass({
       players: this.props.initialPlayers,
     };
   },
+
+  onScoreChange: function(index, delta) {
+    console.log('On Score Change: ', index, delta)
+    this.state.players[index].score += delta;
+    this.setState(this.state);
+  },
   
   render: function() {
     return (
@@ -88,9 +96,15 @@ var Application = React.createClass({
         <Header title={this.props.title}/>
       
         <div className="players">
-        {this.state.players.map(function(player) {
-          return <Player name={player.name} score={player.score} key={player.id} />
-        })}
+        {this.state.players.map(function(player, index) {
+          return (
+          <Player
+            onScoreChange={function(delta) {this.onScoreChange(index ,delta)}.bind(this)}
+            name={player.name}
+            score={player.score}
+            key={player.id} />
+          )
+        }.bind(this))}
         </div>
       
       </div>
